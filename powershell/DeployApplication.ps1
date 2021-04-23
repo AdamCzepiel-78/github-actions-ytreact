5208cd15c50a89755defa68aba43b4c032c4ee44
Adam Czepiel <adam.czepiel@sd-technologies.de>
2021-04-23 04:25:36 +0200

    Powershell Deploy React Application

---
+++ powershell/DeployApplication.ps1
@@ -0,0 +1,23 @@
+Import-Module WebAdministration  
+  
+$iisAppPoolName = "ReactPool"  
+$iisAppPoolDotNetVersion = "v4.0"  
+  
+$iisWebsiteFolderPath = "$(Get-Location)\build"  
+$iisWebsiteName = "ReactApp"  
+  
+$iisWebsiteBindings = @(  
+   @{protocol="http";bindingInformation="*:8000"},  
+)  
+  
+if (!(Test-Path IIS:\AppPools\$iisAppPoolName -pathType container))  
+{  
+New-Item IIS:\AppPools\$iisAppPoolName  
+Set-ItemProperty IIS:\AppPools\$iisAppPoolName -name "managedRuntimeVersion" -value $iisAppPoolDotNetVersion  
+}  
+  
+if (!(Test-Path IIS:\Sites\$iisWebsiteName -pathType container))  
+{  
+New-Item IIS:\Sites\$iisWebsiteName -bindings $iisWebsiteBindings -physicalPath $iisWebsiteFolderPath  
+Set-ItemProperty IIS:\Sites\$iisWebsiteName -name applicationPool -value $iisAppPoolName  
+}
